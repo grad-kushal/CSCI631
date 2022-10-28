554a941edcb4ba3024c96e4595d3192f8644da11
Swetna <48342893+Swetna@users.noreply.github.com>
2022-10-28 19:37:30 -0400

    Add files via upload

---
+++ Project/Project_CheckPoint.m
@@ -0,0 +1,98 @@
+
+Leaf_Classification("/Users/swetna/Documents/MATLAB/Project_Images/IMG_3158.jpeg")
+
+
+
+function Leaf_Classification( fn_in )
+
+%  Get location of raspberries from the user.
+%  Get location of other pixels from the user.
+%
+%  Get color values -- samples - of the raspberries.
+%  Get other color values.
+% 
+%  For each input pixel, figure out distance to the raspberry colors.
+%  For each input pixel, find   distance to the background colors.
+%  If a pixel is closer to the orange color than to a the other,
+
+
+%       classify it as a orange
+%  else
+%       call it a background pixel.
+%
+GET_USER_INPUT = false;
+    
+    if nargin < 1
+        fn_in = fn_in ;
+    end
+    
+    im = im2double( imread( fn_in ) ) ;
+    
+    im  = im( 1:4:end, 1:4:end, : );
+    
+%     zoom_figure( );
+    
+    imagesc( im );
+    axis image;
+    
+  
+        %  Get location of oranges from the user.
+        fprintf('Click on the Leaves\n');
+        beep();
+        [rxs,rys] = ginput();
+
+
+        %  Get location of other pixels from the user.
+        fprintf('Click on the NON-Leaves\n');
+        beep();
+        [bgxs,bgys] = ginput();
+   
+
+        
+    
+    %
+    %  Get color values -- samples - of the oranges.
+    rxs = round( rxs );
+    rys = round( rys );
+    for leaf_idx = 1 : length( rxs )
+        fg_color( leaf_idx, 1:3 ) = im( rys(leaf_idx), rxs(leaf_idx), : );
+    end
+    
+    %  Get other background color values.
+    bgxs = round( bgxs );
+    bgys = round( bgys );
+    for background_idx = 1 : length( bgxs )
+        bg_color( background_idx, 1:3 ) = im( bgys(background_idx), bgxs(background_idx), : );
+    end
+    
+    disp('break here');
+    
+    % 
+    %  For each input pixel, figure out distance to the oranges colors.
+    [im_red, im_grn, im_blu] = imsplit( im );
+    
+    pixel_data = [ im_red(:), im_grn(:), im_blu(:) ];
+    
+    
+    
+    %  For each input pixel, find   distance to the background colors.
+    %  If a pixel is closer to the orange color than to a the other,
+    %       classify it as a orange
+    %  else
+    %       call it a background pixel.
+    %
+   
+   
+    fg_dist = mahal( pixel_data, fg_color );
+    bg_dist = mahal( pixel_data, bg_color );
+    
+    b_is_fg = fg_dist < bg_dist;
+    
+    b_is_fg = reshape( b_is_fg, size(im,1), size(im,2) );
+    
+   figure, imshow( b_is_fg );
+    figure, imagesc( b_is_fg );
+   
+    
+
+end
